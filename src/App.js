5768e1892a027a640dff6246e16882a0cc3ee7e8
Jeremy Tran <nhatphuongb1@gmail.com>
2019-04-19 23:39:49 -0700

    add messenger

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { fbind } from "q";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <div
-          className="fb-customerchat"
-          attribution="setup_tool"
-          page_id="625532594562400"
-          theme_color="#67b868"
-        />
+     
       </div>
     );
   }
