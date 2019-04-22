d016a278fbaa497cb353c9310db8ce85edaf57db
Jeremy Tran <nhatphuongb1@gmail.com>
2019-04-21 20:27:31 -0700

    add messenger-customer-chat library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { fbind } from "q";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 class App extends Component {
   render() {
@@ -23,7 +24,34 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <MessengerCustomerChat
+          pageId="1035675659976102"
+          appId="625532594562400"
+        />
+{/* <div id="fb-root"></div>
 
+   <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId            : '1035675659976102',
+        xfbml            : true,
+        version          : 'v3.2'
+      });
+    };
+  
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+    <div
+  className="fb-customerchat"
+  attribution="setup_tool"
+  page_id="625532594562400"
+  theme_color="#67b868"
+  ></div> */}
       </div>
     );
   }
